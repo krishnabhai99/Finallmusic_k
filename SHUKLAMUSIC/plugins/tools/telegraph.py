d74849a17c4a22e920e65f34eac9231eb8aa875a
Ishatgrepo <dmcomdot@gmail.com>
2025-02-14 17:23:50 +0530

    Update telegraph.py

--- SHUKLAMUSIC/plugins/tools/telegraph.py
+++ SHUKLAMUSIC/plugins/tools/telegraph.py
@@ -58,7 +58,7 @@ async def get_link_group(client, message):
                         [
                             [
                                 InlineKeyboardButton(
-                                    "❍ ᴄʀᴇᴀᴛᴇ ʙʏ ˹ sᴛʀᴀɴɢᴇʀ ™˼",
+                                    "❍ ᴄʀᴇᴀᴛᴇ ʙʏ ˹ HONEY ™˼",
                                     url=upload_path,
                                 )
                             ]
